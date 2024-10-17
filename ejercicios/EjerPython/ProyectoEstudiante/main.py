96ea546a202bcdcbb74c94a31053485be9701154
xamorrod <xamorrod537@ieshnosmachado.org>
2024-10-17 09:53:11 +0200

    Actualizando directorios

--- ejercicios/EjerPython/ProyectoEstudiante/main.py
+++
@@ -1,9 +0,0 @@
-from estudiante import Estudiante 
-from estudianteUtil import crearEstudiante, eliminarEstudiante , obtenerEstudiantes
-from menuEstudiante import menu
-
-
-
-#TODO Arreglar metodo borrado de estudiates
-Estudiante.reload()
-menu()
