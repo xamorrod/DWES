e06a98b04ca4307f9e20730cc7f504ec5b1f6b43
abrahammordev <abrahammordev@gmail.com>
2024-10-27 21:01:46 +0100

    Actualizando tareas pendientes

--- proyectos/BTCTracker/src/main.py
+++ proyectos/BTCTracker/src/main.py
@@ -1,5 +1,11 @@
 from models.readDataFromMultiAdress import getWalletData
 
+
+#TODO Corregir entrada de datos en walletData.json ya que al insertarlos usando las direcciones de las transacciones pendientes no se rellenan
+#TODO Añadir alguna forma de filtrado o búsqueda de direcciones, carteras o saldo extra
+#TODO Implementar interfaz gráfica 
+#TODO Implementar contenerización
+
 def main():
     #readDataFromMultiAdress.getWalletData()
     getWalletData()
