7dcd86b30818395977be4ef82484ff1b04a9895d
abrahammordev <abrahammordev@gmail.com>
2024-11-28 00:17:26 +0100

    Añadiendo parámetro y método

--- ejercicios/ejerDjango/primerproyecto/blog/models.py
+++ ejercicios/ejerDjango/primerproyecto/blog/models.py
@@ -10,8 +10,12 @@ class Post(models.Model):
     autor = models.CharField(max_length=60)
     cuerpo = models.TextField()
     fechaPublicado = models.DateField()
+    email = models.EmailField(unique=True, max_length=200)
 
     # Crear algún procedimiento en relación al modelo
     def __str__(self):
 
-        return str(self.pk) + " " + self.titulo
+        return str(self.pk) + " " + self.titulo 
+    
+    def calcularUser(self):
+        return (f"Este es el usuario: {self.autor[0:2]}{self.email[0:4]}")
